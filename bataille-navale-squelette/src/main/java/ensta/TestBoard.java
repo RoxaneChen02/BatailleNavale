ef600fe7562d90f688c5f9fbf9a72a77be938762
Chen Roxane <chen.roxane19@gmail.com>
2022-02-25 12:34:32 +0100

    final

--- bataille-navale-squelette/src/main/java/ensta/TestBoard.java
+++ bataille-navale-squelette/src/main/java/ensta/TestBoard.java
@@ -7,8 +7,8 @@ import ensta.controller.Game;
 public class TestBoard {
     public static void main(String args[]) {
         Game game = new Game();
-        game = game.init_vs_AI();
-        game.run_vs_AI();
+        game = game.init();
+        game.run();
 
     }
     
